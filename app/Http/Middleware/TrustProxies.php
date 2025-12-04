4942d8de00ebdeb82bee9fb09371c495e646ccf8
ScriptSinger <ufamasters102@gmail.com>
2025-12-04 11:12:24 +0500

    fix: TrustProxies refactoring

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -5,21 +5,14 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
 
-class TrustProxies
+class TrustProxies extends Middleware
 {
-    /**
-     * The trusted proxies for this application.
-     *
-     * @var array<int, string>|string|null
-     */
+
+
     protected $proxies = '*';
 
-    /**
-     * The headers that should be used to detect proxies.
-     *
-     * @var int
-     */
     protected $headers =
     Request::HEADER_X_FORWARDED_FOR |
         Request::HEADER_X_FORWARDED_HOST |
